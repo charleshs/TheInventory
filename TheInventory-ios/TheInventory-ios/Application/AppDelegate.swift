72c9f5a7be0ed2e907334ba2a0d2185eb3d1808e
Charles Hsieh <charlous167@gmail.com>
2021-07-28 09:34:40 +0800

    Create app project for iOS

---
+++ TheInventory-ios/TheInventory-ios/Application/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  TheInventory-ios
+//
+//  Created by Charles Hsieh on 2021/7/27.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        if #available(iOS 13, *) {
+            // Handled by SceneDelegate
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = ViewController()
+            window?.makeKeyAndVisible()
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
