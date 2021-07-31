e91d30d7bf8eaf5b22922cdcbba6f6ef1cc90b3d
Charles Hsieh <charlous167@gmail.com>
2021-08-01 04:20:05 +0800

    Extract "Theme" into a separate module

---
+++ TheInventory/Theme/DefaultTheme.swift
@@ -0,0 +1,16 @@
+//
+//  DefaultTheme.swift
+//  TheInventory-ios
+//
+//  Created by Charles Hsieh on 2021/7/28.
+//
+
+import UIKit
+
+public struct DefaultTheme: Theme {
+
+    public let userInterfaceStyle: UIUserInterfaceStyle = .light
+    public let mainBackground: UIColor = .white
+
+    public init() {}
+}
